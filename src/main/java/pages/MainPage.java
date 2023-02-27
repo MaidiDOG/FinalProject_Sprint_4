1d0434c68125948a07491cc1efbc58e145c87378
Alexander Kosmin <MaidiDOG@yandex.ru>
2023-02-27 21:25:13 +0300

    commit_second_commit

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,25 +1,11 @@
 package pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class MainPage {
     private WebDriver driver;
 
-    private final By btnAccordionItem0 = By.id("accordion__heading-0"); // Локаторы кнопок раздела "Вопросы о важном"
-    private final By btnAccordionItem1 = By.id("accordion__heading-1");
-
-    private final By btnAccordionComponent0 = By.xpath("//p[text()='Сутки — 400 рублей. Оплата курьеру — наличными или картой.']"); //Локаторы с текстом разделов "Вопросы о важном"
-
-    private final By btnAccordionComponent1 = By.xpath("//p[text()='Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.']");
-
-    public static String textOnPanel0 = "Сутки — 400 рублей. Оплата курьеру — наличными или картой."; //Сравниваемый текст
-
-    public static String textOnPanel1 = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."; //Сравниваемый текст (2)
-    private final By btnOrderScooter = By.className("Button_Button__ra12g"); //Кнопка для начала заказа (верхняя)
-    private final By btnOrderScooterBottom = By.className("Button_Button__ra12g"); //Кнопка для начала заказа (нижняя)
     private final By scooterLogo = By.xpath("//img[@alt='Scooter']"); //Логотип со скутера
     private final By homePageBody = By.xpath("//div[@class='Home_HomePage__ZXKIX']"); //Контейинер с элементами главной страницы сервиса "Самокат"
     private final By yandexLogo = By.xpath("//img[@alt='Yandex']"); //Логотип Яндекса
@@ -28,39 +14,6 @@ public class MainPage {
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
-    }
-    // Метод нажимает кнопку, чтобы раскрыть текст в разделе "Вопросы о важном". Первый раздел.
-
-    public void findAndClickBtnAccordionItem0() {
-        WebElement element = driver.findElement(By.id("accordion__heading-0"));
-        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", element);
-        driver.findElement(btnAccordionItem0).click();
-    }
-    //Метод возвращает текст элемента
-    public String checkTextBtnAccordionItem0() {
-       return driver.findElement(btnAccordionComponent0).getText();
-    }
-
-    // Метод нажимает кнопку, чтобы раскрыть текст в разделе "Вопросы о важном". Второй раздел.
-    public void findAndClickBtnAccordionItem1() {
-        WebElement element = driver.findElement(By.id("accordion__heading-1"));
-        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", element);
-        driver.findElement(btnAccordionItem1).click();
-    }
-    //Метод возвращает текст элемента
-    public String checkTextBtnAccordionItem1() {
-        return driver.findElement(btnAccordionComponent1).getText();
-    }
-
-    //Метод для вызова формы заказа самоката (через верхнюю кнопку)
-    public void clickOrderScooterButton() {
-        driver.findElement(btnOrderScooter).click();
-    }
-    //Метод для скрола страницы вниз(до кнопки) и вызова формы заказа самоката (через нижнюю кнопку)
-    public void clickOrderScooterBottomButton() {
-        WebElement element = driver.findElement(By.className("Button_Button__ra12g"));
-        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", element);
-        driver.findElement(btnOrderScooterBottom).click();
     }
     //Метод кликает на логотип и проверяет наличие контейнера с элементом главной страницы сервиса заказа самокатов "Яндекс.Самокат"
     public void checkOnScooterLogoClick() {
@@ -72,6 +25,9 @@ public class MainPage {
         driver.findElement(yandexLogo).click();
         driver.findElement(yandexLogoNewPage).isEnabled();
     }
+
+
+
 
 
 }
--- src/test/java/CheckTextOnMainPage.java
+++ src/test/java/CheckTextOnMainPage.java
@@ -3,38 +3,55 @@ import org.hamcrest.MatcherAssert;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import pages.MainPage;
+import org.openqa.selenium.WebElement;
 
 import static org.hamcrest.CoreMatchers.is;
-import static pages.MainPage.textOnPanel0;
-import static pages.MainPage.textOnPanel1;
 
+@RunWith(Parameterized.class)
 public class CheckTextOnMainPage {
+    private By btnForOpenContainer;
+    private String textInContainer;
+    private By containerWithText;
+    public CheckTextOnMainPage (By btnForOpenContainer, String textInContainer, By containerWithText) {
+        this.btnForOpenContainer = btnForOpenContainer;
+        this.textInContainer = textInContainer;
+        this.containerWithText = containerWithText;
+    }
+    @Parameterized.Parameters
+    public static Object[][] getButtonInfo() {
+        return new Object[][]{
+                {By.id("accordion__heading-0"), "Сутки — 400 рублей. Оплата курьеру — наличными или картой.", By.id("accordion__panel-0")},
+                {By.id("accordion__heading-1"), "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.", By.id("accordion__panel-1")},
+                {By.id("accordion__heading-2"), "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.", By.id("accordion__panel-2")},
+                {By.id("accordion__heading-3"), "Только начиная с завтрашнего дня. Но скоро станем расторопнее.", By.id("accordion__panel-3")},
+                {By.id("accordion__heading-4"), "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.", By.id("accordion__panel-4")},
+                {By.id("accordion__heading-5"), "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.", By.id("accordion__panel-5")},
+                {By.id("accordion__heading-6"), "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.", By.id("accordion__panel-6")},
+                {By.id("accordion__heading-7"), "Да, обязательно. Всем самокатов! И Москве, и Московской области.", By.id("accordion__panel-7")},
+        };
+    }
 
     private WebDriver driver;
 
-    @Before
 
+    @Before
     public void setup() {
         driver = WebDriverFactory.get();
     }
 
     @Test
     public void checkTextOnMainPageInBottomButton0() {
-        MainPage page = new MainPage(driver);
-        page.findAndClickBtnAccordionItem0();
-        String getTextOnPanel0 = page.checkTextBtnAccordionItem0();
-        MatcherAssert.assertThat("После нажатия на стрелочку, открывается соответствующий текст", getTextOnPanel0, is(textOnPanel0)); //Тест проверяет схожесть текста фактического с ожидаемым.
+        WebElement element = driver.findElement(btnForOpenContainer);
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", element);
+        driver.findElement(btnForOpenContainer).click();
+        MatcherAssert.assertThat("После нажатия на стрелочку, открывается соответствующий текст", driver.findElement(containerWithText).getText(), is(textInContainer)); //Тест проверяет схожесть текста фактического с ожидаемым.
     }
 
-    @Test
-    public void checkTextOnMainPageInBottomButton1() {
-        MainPage page = new MainPage(driver);
-        page.findAndClickBtnAccordionItem1();
-        String getTextOnPanel1 = page.checkTextBtnAccordionItem1();
-        MatcherAssert.assertThat("После нажатия на стрелочку, открывается соответствующий текст", getTextOnPanel1, is(textOnPanel1));//Тест проверяет схожесть текста фактического с ожидаемым.
-    }
 
     @After
     public void teardown() {
